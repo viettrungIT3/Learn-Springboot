ca517d85c5955422aefab856956e2cf16b05d232
viettrungIT3 <viettrungcntt03@gmail.com>
2022-07-16 17:20:51 +0700

    learn 1 - Hello World

--- JMasterTTJava/Learn01-HelloWorld/src/main/java/com/example/learn01helloworld/controller/HelloController.java
+++ JMasterTTJava/Learn01-HelloWorld/src/main/java/com/example/learn01helloworld/controller/HelloController.java
@@ -1,2 +1,17 @@
-package com.example.learn01helloworld.controller;public class HelloController {
+package com.example.learn01helloworld.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hi")
+    public String hello(HttpServletRequest request) {
+        request.setAttribute("msg", "Hello World!");
+        return "hello";
+    }
 }
